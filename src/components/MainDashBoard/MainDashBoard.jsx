5039fb15b186e9ca733b0a5998429445292b5daf
RedisculuS <47760888+RedisculuS@users.noreply.github.com>
2024-10-31 14:49:00 +0200

    beginning with strange scrolls

--- src/components/MainDashBoard/MainDashBoard.jsx
+++ src/components/MainDashBoard/MainDashBoard.jsx
@@ -1,14 +1,58 @@
+import SvgIcon from "../SvgIcon/SvgIcon";
 import s from "./MainDashBoard.module.css";
 
 const MainDashBoard = ({ boardId }) => {
+  const columns = [
+    {
+      columnTitle: "Column title 1",
+      cards: [
+        { cardTitle: "Card 1", cardDescr: "Card descr" },
+        { cardTitle: "Card 2", cardDescr: "Card descr" },
+        { cardTitle: "Card 3", cardDescr: "Card descr" },
+      ],
+    },
+    {
+      columnTitle: "Column title 2",
+      cards: [{ cardTitle: "Card 1", cardDescr: "Card descr" }],
+    },
+    {
+      columnTitle: "Column title 3",
+      cards: [
+        { cardTitle: "Card 1", cardDescr: "Card descr" },
+        { cardTitle: "Card 2", cardDescr: "Card descr" },
+        { cardTitle: "Card 3", cardDescr: "Card descr" },
+        { cardTitle: "Card 4", cardDescr: "Card descr" },
+        { cardTitle: "Card 5", cardDescr: "Card descr" },
+        { cardTitle: "Card 6", cardDescr: "Card descr" },
+      ],
+    },
+  ];
+
   return (
     <>
-      <div className={s.cont}>MainDashBoard number {boardId}</div>
-      <ul>
-        <li>Колонки в яких будуть рендеритись картки 1</li>
-        <li>Колонки в яких будуть рендеритись картки 2</li>
-        <li>Колонки в яких будуть рендеритись картки 3 </li>
-      </ul>
+      <div className={s.boardContainer}>
+        {columns.map((column, index) => (
+          <div key={index} className={s.columnWrapper}>
+            <div className={s.columnNameBox}>
+              <div className={s.columnName}>
+                {column.columnTitle} {boardId}
+              </div>
+              <div className={s.iconsBox}>
+                <SvgIcon id="icon-pencil" className={s.columnIcons} />
+                <SvgIcon id="icon-trash" className={s.columnIcons} />
+              </div>
+            </div>
+            <div className={s.cardList}>
+              {column.cards.map((card, cardIndex) => (
+                <div key={cardIndex} className={s.card}>
+                  <h3 className={s.cardTitle}>{card.cardTitle}</h3>
+                  <p className={s.cardDescr}>{card.cardDescr}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
--- src/pages/ScreensPage/ScreensPage.jsx
+++ src/pages/ScreensPage/ScreensPage.jsx
@@ -1,17 +1,25 @@
 import { useParams } from "react-router-dom";
 import s from "./ScreensPage.module.css";
 import MainDashBoard from "../../components/MainDashBoard/MainDashBoard.jsx";
+import SvgIcon from "../../components/SvgIcon/SvgIcon.jsx";
 
 const ScreensPage = () => {
   const { boardId } = useParams();
 
   return (
     <div className={s.cont}>
-      <h2>Board </h2>
-      <p>Нижче створюйте вже колонки {boardId}</p>
-      <p>Десь тут ще хедер борда{boardId}</p>
+      <div className={s.boardTitleBox}>
+        <h2 className={s.boardTitle}>Board name </h2>
+        <div className={s.filterBox}>
+          <SvgIcon id="icon-filter" className={s.filterIcon} />
+          <h3 className={s.filter}>Filters</h3>
+        </div>
+      </div>
+
+      {/* <p>Нижче створюйте вже колонки {boardId}</p>
+      <p>Десь тут ще хедер борда{boardId}</p> */}
       <MainDashBoard boardId={boardId} />
-      <div>Відмалюємо дошку з id : {boardId} з даних запиту</div>
+      {/* <div>Відмалюємо дошку з id : {boardId} з даних запиту</div> */}
     </div>
   );
 };
