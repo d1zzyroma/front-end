86080819cc7055dabc7c4ae44b0945fa1290ee2f
RedisculuS <47760888+RedisculuS@users.noreply.github.com>
2024-11-01 12:47:53 +0200

    cards view and scroll
    Scroll customize and position

--- src/components/MainDashBoard/MainDashBoard.jsx
+++ src/components/MainDashBoard/MainDashBoard.jsx
@@ -31,49 +31,51 @@ const MainDashBoard = ({ boardId }) => {
   return (
     <>
       <div className={s.boardContainer}>
-        {columns.map((column, index) => (
-          <div key={index} className={s.columnWrapper}>
-            <div className={s.columnNameBox}>
-              <div className={s.columnName}>
-                {column.columnTitle} {boardId}
+        <span className={s.scrollBottomContainer}>
+          {columns.map((column, index) => (
+            <div key={index} className={s.columnWrapper}>
+              <div className={s.columnNameBox}>
+                <div className={s.columnName}>
+                  {column.columnTitle} {boardId}
+                </div>
+                <div className={s.iconsBox}>
+                  <SvgIcon id="icon-pencil" className={s.columnIcons} />
+                  <SvgIcon id="icon-trash" className={s.columnIcons} />
+                </div>
               </div>
-              <div className={s.iconsBox}>
-                <SvgIcon id="icon-pencil" className={s.columnIcons} />
-                <SvgIcon id="icon-trash" className={s.columnIcons} />
-              </div>
-            </div>
-            <div className={s.cardList}>
-              {column.cards.map((card, cardIndex) => (
-                <div key={cardIndex} className={s.card}>
-                  <div className={s.cardStatusSpanWrapper}></div>
-                  <h3 className={s.cardTitle}>{card.cardTitle}</h3>
-                  <p className={s.cardDescr}>{card.cardDescr}</p>
-                  <div className={s.cardDivider}></div>
-                  <div className={s.cardEditsInfo}>
-                    <div className={s.cardInfo}>
-                      <div className={s.priority}>
-                        <p className={s.priorityTitle}>Priority</p>
-                        <div className={s.priorityValueWrapper}>
-                          <div className={s.priorityCircle}></div>
-                          <p className={s.priorityValue}>Medium</p>
+              <div className={s.cardList}>
+                {column.cards.map((card, cardIndex) => (
+                  <div key={cardIndex} className={s.card}>
+                    <div className={s.cardStatusSpanWrapper}></div>
+                    <h3 className={s.cardTitle}>{card.cardTitle}</h3>
+                    <p className={s.cardDescr}>{card.cardDescr}</p>
+                    <div className={s.cardDivider}></div>
+                    <div className={s.cardEditsInfo}>
+                      <div className={s.cardInfo}>
+                        <div className={s.priority}>
+                          <p className={s.priorityTitle}>Priority</p>
+                          <div className={s.priorityValueWrapper}>
+                            <div className={s.priorityCircle}></div>
+                            <p className={s.priorityValue}>Medium</p>
+                          </div>
+                        </div>
+                        <div className={s.cardDeadline}>
+                          <p className={s.deadline}>Deadline</p>
+                          <p className={s.deadlineDate}>31/10/2024</p>
                         </div>
                       </div>
-                      <div className={s.cardDeadline}>
-                        <p className={s.deadline}>Deadline</p>
-                        <p className={s.deadlineDate}>31/10/2024</p>
+                      <div className={s.cardIcons}>
+                        <SvgIcon
+                          id="icon-arrow-circle-broken-right"
+                          className={s.columnIcons}
+                        />
+                        <SvgIcon id="icon-pencil" className={s.columnIcons} />
+                        <SvgIcon id="icon-trash" className={s.columnIcons} />
                       </div>
                     </div>
-                    <div className={s.cardIcons}>
-                      <SvgIcon
-                        id="icon-arrow-circle-broken-right"
-                        className={s.columnIcons}
-                      />
-                      <SvgIcon id="icon-pencil" className={s.columnIcons} />
-                      <SvgIcon id="icon-trash" className={s.columnIcons} />
-                    </div>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
               <button className={s.addCardBtn}>
                 <span className={s.btnWrapper}>
                   <span className={s.btnIconWrapper}>
@@ -83,8 +85,8 @@ const MainDashBoard = ({ boardId }) => {
                 </span>
               </button>
             </div>
-          </div>
-        ))}
+          ))}
+        </span>
       </div>
     </>
   );
