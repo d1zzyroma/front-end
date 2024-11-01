1a1e5c7c0fffe8cfa6c74fa14dd4f181d57fd3d8
Anastasiia Tarasiuk <111896564+anastasiiatarasiuk@users.noreply.github.com>
2024-11-01 11:29:59 +0200

    add sidebar

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,5 +1,7 @@
 import { NavLink } from "react-router-dom";
 import s from "./SideBar.module.css";
+import SvgIcon from "../SvgIcon/SvgIcon";
+import cactusImg from "../../images/Sidebar/cactus.png";
 
 const SideBar = () => {
   const boards = [
@@ -10,15 +12,49 @@ const SideBar = () => {
   return (
     <>
       <div className={s.cont}>
-        SideBar
-        <h1>Boards</h1>
-        <ul>
+        <h1 className={s.sidebarTitle}>My boards</h1>
+        <div className={s.createBoard}>
+          <p className={s.createBoardTitle}>Create new board</p>
+          <button type="submit" className={s.addBtn}>
+            <SvgIcon id="icon-plus" className={s.addBtnIcon} />
+          </button>
+        </div>
+        <ul className={s.boardsList}>
           {boards.map((board) => (
-            <li key={board.id}>
-              <NavLink to={`/home/${board.id}`}>{board.name}</NavLink>
+            <li key={board.id} className={s.boardItem}>
+              <NavLink to={`/home/${board.id}`} className={s.link}>
+                <div className={s.linkContent}>
+                  <SvgIcon id="icon-Project" className={s.projectIcon} />
+                  {board.name}
+                </div>
+                <div className={s.btnGroup}>
+                  <button type="button">
+                    <SvgIcon id="icon-pencil" className={s.iconPencil} />
+                  </button>
+                  <button>
+                    <SvgIcon id="icon-trash" className={s.iconTrash} />
+                  </button>
+                </div>
+              </NavLink>
             </li>
           ))}
         </ul>
+        <div className={s.needHelpContainer}>
+          <img src={cactusImg} className={s.cactus} />
+          <p className={s.needHelpText}>
+            If you need help with <span className={s.accent}>TaskPro</span>,
+            check out our support resources or reach out to our customer support
+            team.
+          </p>
+          <button type="button" className={s.needHelpBtn}>
+            <SvgIcon id="icon-help-circle" className={s.needHelpIcon} />
+            Need Help?
+          </button>
+        </div>
+        <button type="button" className={s.logoutBtn}>
+          <SvgIcon id="icon-login" className={s.logoutIcon} />
+          Log out
+        </button>
       </div>
     </>
   );
