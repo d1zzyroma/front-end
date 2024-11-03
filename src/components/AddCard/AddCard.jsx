2f0fc636b95b33b90f464c7f214d96b15921a762
MaksymOvcharenko <153368666+MaksymOvcharenko@users.noreply.github.com>
2024-11-03 13:57:47 +0100

    add calendar

---
+++ src/components/AddCard/AddCard.jsx
@@ -0,0 +1,105 @@
+import s from "./AddCard.module.css";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+const AddCardForm = () => {
+  const initialValues = {
+    title: "",
+    description: "",
+    labelColor: "",
+    deadline: null,
+  };
+  const startDate = Date.now();
+  const validationSchema = Yup.object({
+    title: Yup.string().required("Required"),
+    description: Yup.string(),
+    labelColor: Yup.string().required("Required"),
+    deadline: Yup.date().required("Required"),
+  });
+
+  const handleSubmit = (values) => {
+    console.log("Form Data:", values);
+  };
+  const labelOptions = [
+    { color: "#8fa1d0", priority: "low" },
+    { color: "#e09cb5", priority: "medium" },
+    { color: "#bedbb0", priority: "high" },
+    { color: "rgba(22, 22, 22, 0.3)", priority: "without" },
+  ];
+
+  return (
+    <div className={s.modal}>
+      <div className={s.modalcontent}>
+        <h2 className={s.title}>Add Card</h2>
+        <button className={s.closebtn}>&times;</button>
+
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+        >
+          {({ setFieldValue, values }) => (
+            <Form className={s.form}>
+              <div className={s.input}>
+                <Field name="title" placeholder="Title" className={s.field} />
+              </div>
+
+              <div className={s.input}>
+                <Field
+                  as="textarea"
+                  name="description"
+                  placeholder="Description"
+                  className={s.fielddescr}
+                />
+              </div>
+
+              <div className={s.input}>
+                <div className={s.input}>
+                  <label>Label color</label>
+                  <div className={s.labelColors}>
+                    {labelOptions.map(({ color, priority }) => (
+                      <button
+                        key={color}
+                        type="button"
+                        className={`${s.colorButton} ${
+                          values.labelColor === color ? s.selectedColor : ""
+                        }`}
+                        style={{ color: color, backgroundColor: color }}
+                        onClick={() => {
+                          setFieldValue("labelColor", color);
+                          setFieldValue("priority", priority);
+                        }}
+                      />
+                    ))}
+                  </div>
+                </div>
+              </div>
+
+              <div className={s.input}>
+                <label htmlFor="deadline">Deadline</label>
+                <DatePicker
+                  selected={values.deadline}
+                  onChange={(date) => setFieldValue("deadline", date)}
+                  placeholderText="Select Date"
+                  dateFormat="EEEE, MMMM dd"
+                  minDate={startDate}
+                  showPopperArrow={false}
+                  onFocus={(e) => e.target.blur()}
+                  onKeyDown={(e) => e.preventDefault()}
+                />
+              </div>
+
+              <button type="submit" className="add-button">
+                + Add
+              </button>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </div>
+  );
+};
+
+export default AddCardForm;
--- src/pages/ScreensPage/ScreensPage.jsx
+++ src/pages/ScreensPage/ScreensPage.jsx
@@ -1,17 +1,19 @@
 import { useParams } from "react-router-dom";
 import s from "./ScreensPage.module.css";
 import MainDashBoard from "../../components/MainDashBoard/MainDashBoard.jsx";
+import AddCard from "../../components/AddCard/AddCard.jsx";
 
 const ScreensPage = () => {
   const { boardId } = useParams();
 
   return (
     <div className={s.cont}>
-      <h2 className={s.title}>Board </h2>
+      {/* <h2 className={s.title}>Board </h2>
       <p>Нижче створюйте вже колонки {boardId}</p>
       <p>Десь тут ще хедер борда{boardId}</p>
       <MainDashBoard boardId={boardId} />
-      <div>Відмалюємо дошку з id : {boardId} з даних запиту</div>
+      <div>Відмалюємо дошку з id : {boardId} з даних запиту</div> */}
+      <AddCard />
     </div>
   );
 };
