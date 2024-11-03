e7046f1782f8fde2ac33b44e5171c286a18e93f6
Voroshylo <152655013+Voroshylo@users.noreply.github.com>
2024-11-03 11:11:06 +0200

    header beta

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,48 @@
-import SvgIcon from "../SvgIcon/SvgIcon.jsx";
-import ThemeSwitcher from "../Themes/ThemeSwitcher/ThemeSwitcher.jsx";
+import { useEffect, useState } from "react";
+import icons from "../../images/icons/icons.svg";
+// import ThemeSwitcher from "../Themes/ThemeSwitcher/ThemeSwitcher.jsx";
 import s from "./Header.module.css";
 
 const Header = () => {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+  }, [theme]);
+
+  const handleThemeChange = (event) => {
+    setTheme(event.target.value);
+  };
+
   return (
     <div className={s.cont}>
-      Header
-      <ThemeSwitcher />
-      <SvgIcon id="icon-welcome" className="user-icon" />
+      <svg className={s.mobileMenu}>
+        <use href={`${icons}#icon-bell`} />
+      </svg>
+      <div className={s.divRight}>
+        <div className={s.divLabel}>
+          <label htmlFor="theme-selector" className={s.label}>
+            {/* <p className={s.p}>Theme</p> */}
+            <div className={s.divSelect}>
+              <select
+                name="Theme"
+                id="theme-selector"
+                value={theme}
+                onChange={handleThemeChange}
+                className={s.select}
+              >
+                <option value="light">Light</option>
+                <option value="dark">Dark</option>
+                <option value="violet">Violet</option>
+              </select>
+            </div>
+          </label>
+        </div>
+        <div className={s.divUser}>
+          <p className={s.p}>Login</p>
+          <div className={s.divImg}></div>
+        </div>
+      </div>
     </div>
   );
 };
