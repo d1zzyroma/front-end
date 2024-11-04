ffa058505405ea013dca63163862db7d8d098c4e
Qerilod <140080894+Qerilod@users.noreply.github.com>
2024-11-04 16:12:49 +0200

    comm-1
    не дороблена ще верстка welcome та відправка форми реєстрації, іконка ока під айдішніком #icon-eyes (поміняти якщо не збігається)

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,12 +1,38 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import s from "./WelcomePage.module.css";
 
 const WelcomePage = () => {
+  const [activeButton, setActiveButton] = useState("register"); // За замовчуванням активна кнопка "Register"
+
   return (
     <>
-      <div className={s.cont}>WelcomePage</div>
-      <NavLink to="/auth/login">Login</NavLink>
-      <NavLink to="/auth/register">Register</NavLink>
+      <div className={s.cont}>
+        <h1 className={s.title}>
+          Supercharge your productivity and take control of your tasks with Task
+          Pro - Dont wait, start achieving your goals now!
+        </h1>
+      </div>
+      <div className={s.nav}>
+        <NavLink
+          to="/auth/register"
+          className={`${s.button} ${
+            activeButton === "register" ? s.active : ""
+          }`}
+          onMouseEnter={() => setActiveButton("register")}
+          onFocus={() => setActiveButton("register")}
+        >
+          Registration
+        </NavLink>
+        <NavLink
+          to="/auth/login"
+          className={`${s.button} ${activeButton === "login" ? s.active : ""}`}
+          onMouseEnter={() => setActiveButton("login")}
+          onFocus={() => setActiveButton("login")}
+        >
+          Log in
+        </NavLink>
+      </div>
     </>
   );
 };
