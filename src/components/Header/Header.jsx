cdb80d7c2c31e9fb13621d9def5ba0f32e38b1ce
Voroshylo <152655013+Voroshylo@users.noreply.github.com>
2024-11-05 23:28:09 +0200

    fix modal overlay. userName and Avatar undefine

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,22 @@
 import { useEffect, useState } from "react";
 import ThemeSwitcher from "../Themes/ThemeSwitcher/ThemeSwitcher";
 import EditProfile from "../EditProfile/EditProfile.jsx";
+import { useSelector } from "react-redux";
+import {
+  selectUserName,
+  selectUserAvatar,
+} from "../../redux/auth/selectors.js";
 import icons from "../../images/icons/icons.svg";
 import s from "./Header.module.css";
-import userAva from "../../images/user.png";
+
 const Header = () => {
   const [theme, setTheme] = useState("light");
   const [isOpen, setIsOpen] = useState(false);
   const [isEditProfileOpen, setIsEditProfileOpen] = useState(false);
+  const userName = useSelector(selectUserName);
+  const userAvatar = useSelector(selectUserAvatar);
+  console.log("User Name:", userName);
+  console.log("User Avatar:", userAvatar);
 
   useEffect(() => {
     document.documentElement.setAttribute("data-theme", theme);
@@ -34,9 +43,11 @@ const Header = () => {
   return (
     <header>
       <div className={s.cont}>
-        <svg className={s.mobileMenu}>
-          <use href={`${icons}#icon-mobile-menu`} />
-        </svg>
+        <button className={s.btnMobMenu} type="button">
+          <svg className={s.mobileMenu}>
+            <use href={`${icons}#icon-mobile-menu`} />
+          </svg>
+        </button>
         <div className={s.divRight}>
           <div className={s.divLabel}>
             <p className={s.pTheme}>Theme</p>
@@ -56,14 +67,24 @@ const Header = () => {
             )}
           </div>
           <div className={s.divUser}>
-            <p className={s.p}>UserName</p>
+            <p className={s.p}>{userName}</p>
             <div className={s.divImg}>
               <button
                 className={s.btnEditProf}
                 type="button"
                 onClick={openEditProfile}
               >
-                <img src={userAva} alt="UserAvatar" />
+                {userAvatar ? (
+                  <img
+                    src={userAvatar}
+                    alt="avatar"
+                    className={s.profile_avatar}
+                  />
+                ) : (
+                  <svg className={s.profile_icon}>
+                    <use href={`${icons}#icon-user-ico`}></use>
+                  </svg>
+                )}
               </button>
             </div>
           </div>
