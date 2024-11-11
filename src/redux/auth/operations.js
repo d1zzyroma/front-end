5eb078dfbe373ebbe668ca8e7f4a344dbccb8eef
MaksymOvcharenko <153368666+MaksymOvcharenko@users.noreply.github.com>
2024-11-11 17:10:45 +0100

    туц

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -319,7 +319,7 @@ export const needHelp = createAsyncThunk(
 export const googleLogIn = createAsyncThunk(
   "auth/googleLogin",
   async (data, thunkAPI) => {
-    const code = { code: "4/" + data };
+    const code = { code: data };
     try {
       const res = await taskProApi.post("/auth/verify-oauth", code);
       setAuthHeader(res.data.data.accessToken);
